5d99daf028dd59b49b2edbdeb2b1926675b4a3b1
forrest <forrest@example.com>
2018-10-08 16:12:38 +0800

    添加两数相加

--- data_structure/src/linkeListSummary/Summary.java
+++ data_structure/src/linkeListSummary/Summary.java
@@ -3,7 +3,6 @@ package linkeListSummary;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.Iterator;
 import java.util.List;
 import java.util.TreeSet;
 
@@ -17,11 +16,9 @@ public class Summary {
 			val = x;
 		}
 	}
-	
+
 	/**
-	 * 合并有序（大小排序的）链表
-	 * 输入：1->2->4, 1->3->4
-	 * 输出：1->1->2->3->4->4
+	 * 合并有序（大小排序的）链表 输入：1->2->4, 1->3->4 输出：1->1->2->3->4->4
 	 * 
 	 * @param l1
 	 * @param l2
@@ -29,16 +26,16 @@ public class Summary {
 	 */
 	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
 		List<ListNode> set = new ArrayList<>();
-		while(true) {
-			if(l1!=null) {
+		while (true) {
+			if (l1 != null) {
 				set.add(l1);
 				l1 = l1.next;
 			}
-			if(l2!=null) {
+			if (l2 != null) {
 				set.add(l2);
 				l2 = l2.next;
 			}
-			if(l1 == null && l2 == null) {
+			if (l1 == null && l2 == null) {
 				break;
 			}
 		}
@@ -48,24 +45,66 @@ public class Summary {
 				return o1.val - o2.val;
 			}
 		});
-		for (int j = 0; j < set.size() -1; j++) {
-			set.get(j).next = set.get(j+1);
+		for (int j = 0; j < set.size() - 1; j++) {
+			set.get(j).next = set.get(j + 1);
 		}
-		if(set.isEmpty()) {
+		if (set.isEmpty()) {
 			return null;
 		}
 		return set.get(0);
 	}
 	
-	public static void main(String[] args) {
-		TreeSet<Integer> set = new TreeSet<>();
-		set.add(1);
-		set.add(1);
-		set.add(4);
-		set.add(5);
-		for (Integer integer : set) {
-			System.out.println(integer);
+	/**
+	 * 
+		给定两个非空链表来表示两个非负整数。位数按照逆序方式存储，
+		它们的每个节点只存储单个数字。将两数相加返回一个新的链表。
+		你可以假设除了数字 0 之外，这两个数字都不会以零开头。
+	 * @param l1
+	 * @param l2
+	 * @return
+	 */
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		ListNode result = null;
+		ListNode temp  = null;
+		int j = 0;
+		while(l1!=null||l2!=null) {
+			int x = 0;
+			if(l1!=null&&l2!=null) {
+				x = l1.val + l2.val + j;
+			}else if(l1!=null&&l2 == null) {
+				x = l1.val+j;
+			}else {
+				x = l2.val+j;
+			}
+			if(x>=10) {
+				x = x - 10;
+				j = 1;
+			}else {
+				j = 0;
+			}
+			ListNode temp2 = new ListNode(x);
+			if(result == null) {
+				result = temp2;
+			}else {
+				temp.next = temp2;
+			}
+			temp = temp2;
+			if(l1!=null) {
+				l1 = l1.next;
+			}
+			if(l2!=null) {
+				l2 = l2.next;
+			}
 		}
+		if(j!=0) {
+			ListNode temp2 = new ListNode(1);
+			temp.next = temp2;
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		
 	}
 
 }
