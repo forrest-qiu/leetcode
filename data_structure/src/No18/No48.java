ff22faa724058645d2e40f3810eaeffd616048cb
qiu.yanpeng <qiuyp1@bngrp.com>
2021-04-13 16:57:03 +0800

    55

---
+++ data_structure/src/No18/No48.java
@@ -0,0 +1,29 @@
+package No18;
+
+public class No48 {
+
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n / 2; ++i) {
+            for (int j = 0; j < (n + 1) / 2; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - j - 1][i];
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+                matrix[j][n - i - 1] = temp;
+            }
+        }
+    }
+
+
+    public void rotation(int[][] nums,int c){
+        int length = nums[0].length - 2<<(c-1);
+        for (int i = 0; i < 4; i++) {
+            int[] temp = new int[length];
+            for (int j = 0; j < length; j++) {
+                temp[j] = nums[j][length-c];
+                nums[j][length-c] = nums[c+1][j];
+            }
+        }
+    }
+}
---
+++ data_structure/src/No18/No54.java
@@ -0,0 +1,47 @@
+package No18;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class No54 {
+    /**
+     * 给你一个 m 行 n 列的矩阵 matrix ，请按照 顺时针螺旋顺序 ，返回矩阵中的所有元素。
+     * @param matrix
+     * @return
+     */
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return result;
+        }
+        int row = matrix.length;  //行
+        int column = matrix[0].length;  //列
+        int left = 0;
+        int right = column -1;
+        int bottom = row -1;
+        int top = 0;
+        while(left<=right && top<=bottom){
+            for (int i = left; i <= right ; i++) {
+                result.add(matrix[top][i]);
+            }
+            for (int i = top+1; i <= bottom; i++) {
+                result.add(matrix[i][right]);
+            }
+            if(left<right&&top<bottom){
+                for (int i = right-1; i >left ; i--) {
+                    result.add(matrix[bottom][i]);
+                }
+                for (int i = bottom-1; i > top ; i--) {
+                    result.add(matrix[i][left]);
+                }
+            }
+            left++;
+            right--;
+            bottom--;
+            top++;
+        }
+        return result;
+    }
+
+
+}
