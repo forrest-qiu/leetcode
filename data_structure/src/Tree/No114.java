0d936be41c8b0ef7bdb75b66e15f49c9a12da42a
qiu.yanpeng <qiuyp1@bngrp.com>
2021-04-27 11:23:01 +0800

    二叉树

---
+++ data_structure/src/Tree/No114.java
@@ -0,0 +1,24 @@
+package Tree;
+
+import entity.TreeNode;
+
+/**
+ * 给你二叉树的根结点 root ，请你将它展开为一个单链表：
+ *
+ * 展开后的单链表应该同样使用 TreeNode ，其中 right 子指针指向链表中下一个结点，而左子指针始终为 null 。
+ * 展开后的单链表应该与二叉树 先序遍历 顺序相同。
+ *
+ */
+public class No114 {
+
+    public void flatten(TreeNode root) {
+        if(root == null) return;
+        flatten(root.left);
+        flatten(root.right);
+        TreeNode tmp = root.right;
+        root.right = root.left;
+        root.left = null;
+        while(root.right != null) root = root.right;
+        root.right = tmp;
+    }
+}
